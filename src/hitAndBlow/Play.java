834397cb28429889205e6fe34cef0be13ce6ed35
nagimikan <minami.94330@gmail.com>
2020-04-03 17:46:38 +0900

    Playクラスにターン数をカウントする機能を追加

--- src/hitAndBlow/Play.java
+++ src/hitAndBlow/Play.java
@@ -11,6 +11,9 @@ public class Play {
 
 		// ヒットの数を初期化
 		int hitCount = 0;
+		// ターンの回数を初期化
+		int turnCount = 0;
+
 		for(int i = 1; !isFinished(hitCount); i++) {
 			// 標準入力から値を取得する
 			int[] inputAnswerNum = Input.inputAnswer();
@@ -22,8 +25,11 @@ public class Play {
 			// ブロウの数を取得する
 			int blowCount = Count.countBlow(correctAnswerNum, inputAnswerNum);
 			System.out.println("ブロウ：" + blowCount);
+
+			turnCount += 1;
 		}
 		System.out.println("おわり");
+		System.out.println(turnCount);
 
 	}
 	private static boolean isFinished(int hitCount) {
